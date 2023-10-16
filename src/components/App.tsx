80afe26878aef93544fdbddf251aab459902c55d
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-16 15:57:28 +0400

    pre_done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ReactComponent as Logo} from '../logo.svg';
 import s from '../style/App.module.scss';
 import {Block} from "./Block";
 import {Particle} from "./Particle";
 import {Input} from "./Input";
+import {timeCount} from "../utils/timeCount";
 
 function App() {
 
-    const dataCB = (startData: {time: string, pressure: string}) => {
-        console.log(startData)
-    }
+    let [data, setData] = useState<{ time: string, pressure: string }>({time: '', pressure: ''})
+    const dataCB = (startData: { time: string, pressure: string }) => setData(startData);
+
+    const P_Max_Falling = Math.floor(+data.pressure / 3);
+    const P_Exit = Math.floor(+data.pressure - P_Max_Falling);
+    const Delta_T = Math.floor(P_Max_Falling * 6.8 / 45);
+    const T_Exit = timeCount(data.time, `00:${Delta_T.toString()}`)
+    const T_General = Math.floor(+data.pressure * 6.8 / 45)
+    const T_Returning = timeCount(data.time, `00:${T_General.toString()}`)
+    const T_Fact_Returning = timeCount(data.time, `00:30`)
 
     return (
         <div className={s.app}>
@@ -24,18 +32,31 @@ function App() {
                     <Block title={'Место'} columNumber={2}/>
                     <Block title={'Подразделение, Постовой'} columNumber={3}/>
                     <Block title={'Состав звена'} columNumber={4}/>
-                    <Block title={'T вкл.'} columNumber={5}/>
+                    <Block title={'T вкл.'} columNumber={5} timeOn={data.time}/>
                     <Block title={'P вкл.'} columNumber={6}/>
-                    <Block title={'P max.пад.'} columNumber={7}/>
-                    <Block title={'P к.вых.'} columNumber={8}/>
-                    <Block title={'ΔT'} columNumber={9}/>
-                    <Block title={'T вых.'} columNumber={10}/>
-                    <Block title={'T общ.'} columNumber={11}/>
-                    <Block title={'T возвр.'} columNumber={12}/>
-                    <Block title={'T ф.возвр.'} columNumber={13}/>
-                    <Block title={'Доклады и время'} columNumber={14}/>
+                    <Block title={'P max.пад.'} columNumber={7} P_Max_Falling={P_Max_Falling}/>
+                    <Block title={'P к.вых.'} columNumber={8} P_Exit={P_Exit}/>
+                    <Block title={'ΔT'} columNumber={9} Delta_T={Delta_T}/>
+                    <Block title={'T вых.'} columNumber={10} T_Exit={T_Exit}/>
+                    <Block title={'T общ.'} columNumber={11} T_General={T_General}/>
+                    <Block title={'T возвр.'} columNumber={12} T_Returning={T_Returning}/>
+                    <Block title={'T ф.возвр.'} columNumber={13} T_Fact_Returning={T_Fact_Returning}/>
+                    <Block title={'Доклады и время'} columNumber={14} timeOn={data.time}/>
                 </section>
             </main>
+            <footer className={s.footer}>
+                <a href={'https://t.me/kravtsov_ilia'} className={s.getHireLink} target={'_blank'}>
+                    <div className={s.getHireContainer}>
+                        <div className={s.getHireTitle}>
+                            <span>Контакт для связи</span>
+                        </div>
+                        <img
+                            src={'https://ouch-cdn2.icons8.com/kSIAJQ2detV1XffgFuJ10fUEsh4Gm2atdnFRLFz3ORI/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTg1/L2I5MDg2ZjUyLTRi/NWMtNDYyZC1iMTli/LTk5Y2Q0NGZiYjI4/Yi5wbmc.png'}
+                            className={s.getHireIcon}
+                            alt={'aboutIcon'}/>
+                    </div>
+                </a>
+            </footer>
         </div>
     );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,6 @@
 import React, {ChangeEvent, useState} from 'react';
-import {styled, TextField} from "@mui/material";
 import s from '../style/Input.module.scss'
+import ReactTyped from "react-typed";
 
 type InputType = {
     dataCB: (startData: { time: string, pressure: string }) => void
@@ -11,30 +11,45 @@ export const Input: React.FC<InputType> = ({dataCB}) => {
     let [startData, setStartData] = useState<{time: string, pressure: string}>({time: '', pressure: ''});
     let [timeError, setTimeError] = useState<boolean>(false)
     let [pressureError, setPressureError] = useState<boolean>(false)
+    let [emptyError, setEmptyError] = useState<boolean>(false)
 
     const timeChanger = (e: ChangeEvent<HTMLInputElement>) => {
         setStartData({...startData, time: e.currentTarget.value});
     }
     const pressureChanger = (e: ChangeEvent<HTMLInputElement>) => setStartData({...startData, pressure: e.currentTarget.value});
+    const errorHandler = () => {
+        setEmptyError(true)
+        setTimeout(() => {
+            setEmptyError(false)
+        }, 4000)
+    }
     const onCountClick = () => {
-        const pattern = /^([01]\d|2[0-3]):[0-5]\d$/;
-        if (pattern.test(startData.time)) {
-            if (+startData.pressure < 250 || +startData.pressure > 300) {
-                setPressureError(true)
-            } else {
-                dataCB(startData);
-                setPressureError(false)
-                setStartData({time: '', pressure: ''})
-            }
-            setTimeError(false)
+        if (startData.time === '' && startData.pressure === '') {
+            errorHandler()
+        } else if (startData.time === '' || startData.pressure === '') {
+            errorHandler()
         } else {
-            setTimeError(true)
+            const pattern = /^([01]\d|2[0-3]):[0-5]\d$/;
+            if (pattern.test(startData.time)) {
+                if (+startData.pressure < 250 || +startData.pressure > 300) {
+                    setPressureError(true)
+                } else {
+                    dataCB(startData);
+                    setPressureError(false)
+                    setStartData({time: '', pressure: ''})
+                }
+                setTimeError(false)
+            } else {
+                setTimeError(true)
+            }
         }
     }
 
     return (
         <div className={s.inputContainer}>
-            <h1>Введите исходные данные</h1>
+            <h1>
+                <ReactTyped strings={["Введите исходные данные"]} typeSpeed={100} loopCount={1} />
+            </h1>
             <div className={s.inputSection}>
                 <div className={s.inputBox}>
                     <input value={startData.time}
@@ -42,7 +57,7 @@ export const Input: React.FC<InputType> = ({dataCB}) => {
                            className={s.inputData}
                            type={'text'}
                            required/>
-                    <span>Время включения в формате: 10:00</span>
+                    <span>T вкл. в формате: 10:00</span>
                 </div>
                 <div className={s.inputBox}>
                     <input value={startData.pressure}
@@ -50,7 +65,7 @@ export const Input: React.FC<InputType> = ({dataCB}) => {
                            className={s.inputData}
                            type={'number'}
                            required/>
-                    <span>Минимальное давление P вкл.</span>
+                    <span>{'250 > P min < 300'}</span>
                 </div>
             </div>
             <button type={'submit'} className={s.countClick} onClick={onCountClick}>
@@ -58,8 +73,9 @@ export const Input: React.FC<InputType> = ({dataCB}) => {
                 <div className={s.liquid}></div>
             </button>
             <div className={s.errorBlock}>
-                {timeError && <div className={s.timeError}>Не верно указан формат времени</div>}
-                {pressureError && <div className={s.timeError}>{'250 > P < 300'}</div>}
+                {timeError && <div className={s.error}>Не верно указан формат времени</div>}
+                {pressureError && <div className={s.error}>{'250 > P < 300'}</div>}
+                {emptyError && <div className={s.error}>{'Заполните все поля'}</div>}
             </div>
         </div>
     );
